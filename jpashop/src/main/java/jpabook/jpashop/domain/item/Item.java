a2543155da07bce624f2726783648caaf626fb84
ckr8305 <ckr8305@naver.com>
2024-09-19 18:57:41 +0900

    3주차 과제

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,8 +2,8 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +12,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
     @Id
@@ -27,4 +26,22 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //== 비즈니스 로직 ==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.sockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.sockQuantity - quantity;
+        if (restStock < 0)
+            throw new NotEnoughStockException("need more stock");
+        this.sockQuantity = restStock;
+    }
 }
