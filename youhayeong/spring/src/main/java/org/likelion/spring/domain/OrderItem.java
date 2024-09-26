6996c29fb9e1ee5d96e6a155fa6e5841613e80d5
ttttkii913 <dbgkdud821@daum.net>
2024-09-26 18:27:33 +0900

    4주차 실습

--- youhayeong/spring/src/main/java/org/likelion/spring/domain/OrderItem.java
+++ youhayeong/spring/src/main/java/org/likelion/spring/domain/OrderItem.java
@@ -1,12 +1,15 @@
 package org.likelion.spring.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.likelion.spring.domain.item.Item;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -22,4 +25,25 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    // 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // 주문 상품 전체 가격 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
