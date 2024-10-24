9ec66989daab8f6e1933689733ce64b53e4da7ee
ckr8305 <ckr8305@naver.com>
2024-10-24 17:06:43 +0900

    6주차 과제

--- kyulim_jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ kyulim_jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -39,4 +39,12 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+    
+    
+    // jpa 활용2
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
